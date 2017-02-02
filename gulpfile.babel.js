4f1d2fcb534d51137c5db3e06c7524cfe4b81aa7
Andrés Villanueva <andresvillanuevab@gmail.com>
2017-02-02 14:03:34 -0300

    chore(build): server

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,12 +3,26 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
 const concat = require('gulp-concat');
+const browserSync = require('browser-sync').create();
 const sourcemap = require('gulp-sourcemaps');
 
 
 
-gulp.task('default', () => {
+gulp.task('bundleJs', () => {
     return gulp.src('app/src/js/app.js')
         .pipe(babel())
         .pipe(gulp.dest('app/dist/js'))
 });
+
+gulp.task('server', () => {
+    browserSync.init({
+        open: false,
+        port: 3001,
+        reloadOnRestart: true,
+        server: './app',
+        ui: false
+    })
+
+});
+
+gulp.task('default', ['bundleJs','server']);
