aa1f9f7bc54b71464ee7108960cef741092abf63
Andrés Villanueva <andresvillanuevab@gmail.com>
2017-02-02 01:08:42 -0300

    feature(gulp): gulp with babel

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,3 +1,14 @@
-/**
- * Created by avillanueva on 2/2/17.
- */
+'use strict';
+
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const concat = require('gulp-concat');
+const sourcemap = require('gulp-sourcemaps');
+
+
+
+gulp.task('default', () => {
+    return gulp.src('app/src/js/app.js')
+        .pipe(babel())
+        .pipe(gulp.dest('app/dist/js'))
+});
