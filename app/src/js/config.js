cf067374a99bd182aa324842768bfba2f21a37d2
Andrés Villanueva <andresvillanuevab@gmail.com>
2017-02-05 16:32:47 -0300

    chore(auth): authentication and config

--- app/src/js/config.js
+++ app/src/js/config.js
@@ -1,9 +1,10 @@
 'use strict';
 
-export let config = {
+export const firebase = {
     apiKey: "AIzaSyBsaAOe37sS_D_e0q-1Ib8Q4t8XoeUydo4",
     authDomain: "villanuevand-labs.firebaseapp.com",
     databaseURL: "https://villanuevand-labs.firebaseio.com",
     storageBucket: "villanuevand-labs.appspot.com",
     messagingSenderId: "24781245035"
 };
+
