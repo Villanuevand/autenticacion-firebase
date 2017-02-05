9d4214917da5e00f9f99ea9f5346423d44373c90
Andrés Villanueva <andresvillanuevab@gmail.com>
2017-02-05 18:46:13 -0300

    feature(github): authentication with Github

--- app/src/js/app.js
+++ app/src/js/app.js
@@ -2,9 +2,11 @@
 
 const firebaseConfig =  require('./config').firebase;
 const google = require('./Authentication').googleAuth;
+const github = require('./Authentication').githubAuth;
 
 
 const googleBtn = document.getElementById('google');
+const githubBtn = document.getElementById('github');
 
 const initApp = () => {
     firebase.initializeApp(firebaseConfig);
@@ -13,4 +15,5 @@ const initApp = () => {
 // Initialize Firebase
 initApp();
 
-googleBtn.addEventListener('click', google ,false );
+googleBtn.addEventListener('click', google ,false);
+githubBtn.addEventListener('click', github, false);
