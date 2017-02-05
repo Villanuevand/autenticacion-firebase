6f6f27dca46f155a97ab8ec57219abd5a34c0aea
Andrés Villanueva <andresvillanuevab@gmail.com>
2017-02-05 01:21:52 -0300

    refactor(webpack):add implementation

---
+++ app/src/js/firebaseConfig.js
@@ -0,0 +1,9 @@
+'use strict';
+
+export let config = {
+    apiKey: "AIzaSyBsaAOe37sS_D_e0q-1Ib8Q4t8XoeUydo4",
+    authDomain: "villanuevand-labs.firebaseapp.com",
+    databaseURL: "https://villanuevand-labs.firebaseio.com",
+    storageBucket: "villanuevand-labs.appspot.com",
+    messagingSenderId: "24781245035"
+};
