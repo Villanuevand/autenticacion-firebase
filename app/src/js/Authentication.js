680891fa7474fc8301fe20dd36bd5f326c5deee5
Andrés Villanueva <andresvillanuevab@gmail.com>
2017-02-19 01:47:09 -0300

    refactor(app): change implementation in auth methods.

--- app/src/js/Authentication.js
+++ app/src/js/Authentication.js
@@ -1,18 +1,22 @@
 
+const userLayer = document.getElementById('active-user'),
+    loginLayer = document.getElementById('login'),
+    userName = document.getElementById('userName'),
+    userEmail = document.getElementById('userEmail'),
+    userImage = document.getElementById('userImage');
+
+
 export function googleAuth () {
     "use strict";
-    console.log('googleAuth');
-    const provider = new firebase.auth.GoogleAuthProvider();
-    firebase.auth().signInWithPopup(provider)
-        .then(function(result) {
-            console.log('googleAuth OK');
-            let token = result.credential.accessToken;
-            let user = result.user;
-
-            console.log(token);
-            console.log(user);
-            // ...
-        }).catch(function(error) {
+    if(!firebase.auth().currentUser) {
+        const provider = new firebase.auth.GoogleAuthProvider();
+        firebase.auth().signInWithPopup(provider)
+            .then(function(result) {
+                console.log('googleAuth OK');
+                let token = result.credential.accessToken;
+                let user = result.user;
+                showUserData('google',user,token);
+            }).catch(function(error) {
             console.log('googleAuth Fail');
             // Handle Errors here.
             let errorCode = error.code;
@@ -23,70 +27,108 @@ export function googleAuth () {
             let credential = error.credential;
             // ...
         });
+    } else {
+        console.log('something is wrong');
+        signOut();
+    }
+
+
 }
 
 export function githubAuth () {
     "use strict";
-    console.log('githubAuth');
-    const provider = new firebase.auth.GithubAuthProvider();
-    provider.addScope('user');
-    firebase.auth().signInWithPopup(provider)
-        .then((result) => {
-            console.log('githubAuth OK');
-            let token = result.credential.accessToken;
-            let user = result.user;
+    if (!firebase.auth().currentUser) {
+        const provider = new firebase.auth.GithubAuthProvider();
+        provider.addScope('user');
+        firebase.auth().signInWithPopup(provider)
+            .then((result) => {
+                console.log('githubAuth OK');
+                let token = result.credential.accessToken;
+                let user = result.user;
+                showUserData('github',user,token);
 
-            console.log(token);
-            console.log(user);
+            })
+            .catch((error) => {
+                console.log('githubAuth Fail');
+                // Handle Errors here.
+                let errorCode = error.code;
+                let errorMessage = error.message;
+                // The email of the user's account used.
+                let email = error.email;
+                // The firebase.auth.AuthCredential type that was used.
+                let credential = error.credential;
 
-        })
-        .catch((error) => {
-            console.log('githubAuth Fail');
-            // Handle Errors here.
-            let errorCode = error.code;
-            let errorMessage = error.message;
-            // The email of the user's account used.
-            let email = error.email;
-            // The firebase.auth.AuthCredential type that was used.
-            let credential = error.credential;
+                console.log('errorCode ',errorCode );
+                console.log('errorMessage ',errorMessage );
+                console.log('email ',email );
+                console.log('credential ',credential );
+                // ...
+            });
 
-            console.log('errorCode ',errorCode );
-            console.log('errorMessage ',errorMessage );
-            console.log('email ',email );
-            console.log('credential ',credential );
-            // ...
-        });
+    } else {
+        console.log('something is wrong');
+        signOut();
+    }
+
 }
 
 export function twitterAuth () {
     "use strict";
-    console.log('twitterAuth');
-    const provider = new firebase.auth.TwitterAuthProvider();
-    firebase.auth().signInWithPopup(provider)
-        .then((result) => {
-            console.log('twitterAuth OK');
-            let token = result.credential.accessToken;
-            let user = result.user;
+    if (!firebase.auth().currentUser) {
+        const provider = new firebase.auth.TwitterAuthProvider();
+        firebase.auth().signInWithPopup(provider)
+            .then((result) => {
+                console.log('twitterAuth OK');
+                let token = result.credential.accessToken;
+                let user = result.user;
+                showUserData('twitter',user,token);
+            })
+            .catch((error) => {
+                console.log('twitterAuth Fail');
+                // Handle Errors here.
+                let errorCode = error.code;
+                let errorMessage = error.message;
+                // The email of the user's account used.
+                let email = error.email;
+                // The firebase.auth.AuthCredential type that was used.
+                let credential = error.credential;
 
-            console.log(token);
-            console.log(user);
+                console.log('errorCode ',errorCode );
+                console.log('errorMessage ',errorMessage );
+                console.log('email ',email );
+                console.log('credential ',credential );
+                // ...
+            });
+    } else {
+        console.log('something is wrong');
+        signOut();
+    }
 
-        })
-        .catch((error) => {
-            console.log('twitterAuth Fail');
-            // Handle Errors here.
-            let errorCode = error.code;
-            let errorMessage = error.message;
-            // The email of the user's account used.
-            let email = error.email;
-            // The firebase.auth.AuthCredential type that was used.
-            let credential = error.credential;
+}
 
-            console.log('errorCode ',errorCode );
-            console.log('errorMessage ',errorMessage );
-            console.log('email ',email );
-            console.log('credential ',credential );
-            // ...
-        });
+function showUserData (platform,user,token) {
+    "use strict";
+    console.log('Platform', platform);
+    console.log('Token: ',token);
+    console.dir(user);
 
+    if (user) {
+        userLayer.style.display = 'block';
+        loginLayer.style.display = 'none';
+        userName.innerHTML = user.displayName;
+        userEmail.innerHTML = user.email;
+        userImage.setAttribute('src',user.photoURL);
+    }
+}
+
+
+export function signOut () {
+    "use strict";
+    firebase.auth().signOut().then(function() {
+        console.log('Signed Out');
+        userLayer.style.display = 'none';
+        loginLayer.style.display = 'block';
+    }, function(error) {
+        console.error('Sign Out Error', error);
+    });
 }
--- app/src/js/app.js
+++ app/src/js/app.js
@@ -4,15 +4,18 @@ const firebaseConfig =  require('./config').firebase;
 const google = require('./Authentication').googleAuth;
 const github = require('./Authentication').githubAuth;
 const twitter = require('./Authentication').twitterAuth;
+const logout = require('./Authentication').signOut;
 
 
 const googleBtn = document.getElementById('google');
 const githubBtn = document.getElementById('github');
 const twitterBtn = document.getElementById('twitter');
+const logoutBtn = document.getElementById('logout');
 
 const initApp = () => {
     firebase.initializeApp(firebaseConfig);
 };
+
 
 // Initialize Firebase
 initApp();
@@ -20,3 +23,4 @@ initApp();
 googleBtn.addEventListener('click', google ,false);
 githubBtn.addEventListener('click', github, false);
 twitterBtn.addEventListener('click', twitter, false);
+logoutBtn.addEventListener('click', logout, false);
